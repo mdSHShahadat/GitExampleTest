c153874b9a6930d27c9c97a46345542efb0e1dec
proMdShahadat <pro.mdshahadat@gmail.com>
2019-02-25 00:00:45 +0600

    initial commit

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Please Enter Your Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name);
+            Console.ReadKey();
+
+        }
+    }
+}
